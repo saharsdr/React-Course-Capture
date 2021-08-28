959bd1275667f3557433778bd4c918f7757e3a3b
Sahar <sahar.sadrii.ss@gmail.com>
2021-08-28 16:01:39 +0430

    4. FAQ Section

---
+++ src/components/FaqSections.js
@@ -0,0 +1,52 @@
+
+
+const FaqSection = () => {
+
+  return (
+    <div className="section-faq">
+      <h2>Any Questions <span>FAQ</span></h2>
+      <div className="question">
+        <h4>How Do I Start?</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum corrupti laboriosam rerum iusto atque iure cumque. Voluptate, pariatur explicabo?  
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum corrupti laboriosam rerum iusto atque iure cumque. Voluptate, pariatur explicabo?  
+          </p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Diffrent pay method</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum corrupti laboriosam rerum iusto atque iure cumque. Voluptate, pariatur explicabo?  
+          </p>
+        </div>
+      </div>
+
+
+      <div className="question">
+        <h4>What Products do you offers</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem earum corrupti laboriosam rerum iusto atque iure cumque. Voluptate, pariatur explicabo?  
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FaqSection;
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,16 +1,16 @@
 // Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSections";
 
 const AboutUs = () => {
-
   return (
     <>
-      <AboutSection/>
-      <ServicesSection/>
-
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
     </>
-  )
-}
+  );
+};
 
 export default AboutUs;
