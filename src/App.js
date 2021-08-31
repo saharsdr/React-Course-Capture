f559d362f08b7a9f7f2fd79975743012d0ecd1f8
Sahar <sahar.sadrii.ss@gmail.com>
2021-08-31 10:21:06 +0430

    Navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import GlobalStyle from "./components/GlobalStyle";
 // import "./css/style.css";
 //  Import Pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   );
